8181ecf53497e24287d55968f49b131cdc039f3b
Your Name <amitpratapsingh0000@gmail.com>
2021-01-19 16:32:04 +0530

    refactoring and setting using setter

--- composition/src/Main.java
+++ composition/src/Main.java
@@ -4,9 +4,11 @@ public class Main {
         Dimension dimTeacher = new Dimension(76, 55);
         Dimension dimStudent = new Dimension(56, 75);
         Teacher t1 = new Teacher("MathsTeacher", dimTeacher, "Maths", 32);
-        Student s1 = new Student("Abhishek", dimStudent, 24, new Marks(98,89,100));
+        Student s1 = new Student("Abhishek", dimStudent, 24, new Marks(98, 89, 100));
 
-     School sch = new School(t1, s1);
+        School sch = new School(t1, s1);
+
+        sch.getStudent().getMarks().setMaths(45);
     }
 
 }
--- composition/src/Student.java
+++ composition/src/Student.java
@@ -1,4 +1,4 @@
-public class Student  extends Person{
+public class Student extends Person {
     private int rollNumber;
     private Marks marks;
 
